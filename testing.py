b69115e6576f40f3d2f9d0473e02583e62cf4df1
coderwhale <69923950+coder-whale@users.noreply.github.com>
2021-01-01 22:47:01 +0530

    Successfully tested establishment of connection to api and obtaining data from it
    Also tweaked the code to obtain sql login credentials from environment variables instead of providing it as raw text to the program

--- testing.py
+++ testing.py
@@ -2,21 +2,39 @@ from flask import Flask,jsonify,render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
 import json
+import http.client
+import os
 
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+
+#Can type the values here or obtain them by setting them as environment variables. The latter is done for security purposes
+#app.config['MYSQL_DATABASE_USER'] = 'username'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+
+app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_USERNAME']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_PASSWORD']
 app.config['MYSQL_DATABASE_DB'] = 'footballproject'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
+#Obtaining key to access api from a text file stored in the project root. Can also directly input it as text in the headers of the api connection request
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(ROOT_DIR, 'api_key.txt')) as f:
+    API_KEY = f.read().strip()
 conn = mysql.connect()
 cursor=conn.cursor()
+apiconn=http.client.HTTPSConnection("v3.football.api-sports.io")
+headers={'x-rapidapi-host':"v3.football.api-sports.io",'x-rapidapi-key':API_KEY}
 
+apiconn.request("GET","/fixtures?league=39&team=33&last=10",headers=headers)
 cursor.execute('SELECT * FROM TEST;')
 l=list(cursor)
 print(l)
+res = apiconn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
 
 @app.route('/')
 def hello_world():
