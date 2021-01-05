651e74f7b195c4dc5b85c416ff364bd336fda748
coderwhale <69923950+coder-whale@users.noreply.github.com>
2021-01-05 18:38:37 +0530

    Implemented auto update of fixture data depending on date last updated

--- testing.py
+++ testing.py
@@ -6,6 +6,34 @@ import http.client
 import os
 import datetime
 
+def update_fixtures():
+    cursor.execute('DELETE FROM fixtures_test')
+    conn.commit()
+    apiconn.request("GET","/fixtures?league=39&team=33&last=5",headers=headers)
+    result1 = apiconn.getresponse()
+    data1 = result1.read()
+    apiconn.request("GET","/fixtures?league=39&team=33&next=5",headers=headers)
+    result2 = apiconn.getresponse()
+    data2 = result2.read()
+    matchdata = json.loads(data1.decode("utf-8"))['response'] + json.loads(data2.decode("utf-8"))['response']
+    
+    sqlinsertdata =[]
+    for row in matchdata:
+        templist=[]
+        templist.append(row['fixture']['id'])
+        templist.append(row['fixture']['date'][:10])
+        templist.append(33)
+        templist.append(row['teams']['home']['name'])
+        templist.append(row['teams']['away']['name'])
+        templist.append(row['fixture']['status']['short'])
+        templist.append(row['teams']['home']['winner'])
+        temptuple=tuple(templist)
+        sqlinsertdata.append(temptuple)
+    
+    sqlcmd="INSERT INTO fixtures_test VALUES(%s,%s,%s,%s,%s,%s,%s)"
+    cursor.executemany(sqlcmd,sqlinsertdata)
+    conn.commit()
+
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
 
@@ -29,6 +57,14 @@ apiconn=http.client.HTTPSConnection("v3.football.api-sports.io")
 headers={'x-rapidapi-host':"v3.football.api-sports.io",'x-rapidapi-key':API_KEY}
 
 date=datetime.date.today()
+with open(os.path.join(ROOT_DIR,'fixtures_data.json'),'r+') as f:
+    data=json.load(f)
+    if data['lastUpdated']!=str(date):
+        update_fixtures()
+        data['lastUpdated']=str(date)
+        f.seek(0)
+        json.dump(data,f)
+        f.truncate()
 cursor.execute('SELECT * FROM fixtures_test WHERE matchdate<%s;',(date))
 finishedmatches=list(cursor)
 cursor.execute('SELECT * FROM fixtures_test WHERE matchdate>%s;',(date))
@@ -49,6 +85,6 @@ def ongoing_page():
 @app.route('/upcoming')
 def upcoming_page():
     return render_template('test.html',result = upcomingmatches)
-
+    
 if __name__=='__main__':
     app.run()
